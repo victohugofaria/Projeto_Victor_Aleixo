b614455b17671f2d3e3dc22e0085ae19e0a4bf9f
victorhugofaria <victorhugo2023faria@gmail.com>
2026-09-22 20:30:13 -0300

    Adiciona novo método a B

--- B.java
+++ B.java
@@ -9,4 +9,5 @@ public class B {
     
     public void MB1() { System.out.println("Método MB1"); }
     public void MB2() { System.out.println("Método MB2"); }
+    public void MB3() { System.out.println("Método MB3"); }
 }
