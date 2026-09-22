c600e26f08caa1be864b2033806eab7aae493436
victorhugofaria <victorhugo2023faria@gmail.com>
2026-09-22 20:30:47 -0300

    Adiciona novo método a C

--- C.java
+++ C.java
@@ -3,4 +3,5 @@ public class C {
     private int C2;
     public void MC1() { System.out.println("Método MC1"); }
     public void MC2() { System.out.println("Método MC2"); }
+    public void MC3() { System.out.println("Método MC3"); }
 }
