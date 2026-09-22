9ec7872976f2921e19cfcf9e9555bc2240dee7b3
victorhugofaria <victorhugo2023faria@gmail.com>
2026-09-22 19:19:47 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,6 @@
+public class C {
+    private String C1;
+    private int C2;
+    public void MC1() { System.out.println("Método MC1"); }
+    public void MC2() { System.out.println("Método MC2"); }
+}
