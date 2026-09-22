99e6fa8b9d88448f0638ad7c9442b7192aca8198
victorhugofaria <victorhugo2023faria@gmail.com>
2026-09-22 20:31:27 -0300

    Criação da classe D

---
+++ D.java
@@ -0,0 +1,6 @@
+public class D {
+    private double D1;
+    private double D2;
+    public void MD1() { System.out.println("Método MD1"); }
+    public void MD2() { System.out.println("Método MD2"); }
+}
